6e57e9fe9d29345ff06f39732812bb3495bae14c
IlhamShiddiq <shdqillham123@gmail.com>
2023-03-29 13:51:40 +0700

    feat: test success

--- src/scripts/states/leaderboards/action.js
+++ src/scripts/states/leaderboards/action.js
@@ -21,7 +21,7 @@ const asyncReceiveLeaderBoards = () => {
       const leaderboards = await api.getLeaderboards()
       dispatch(receiveLeaderboardsActionCreator(leaderboards))
     } catch (error) {
-      alert(error.messsage)
+      alert(error.message)
     }
     dispatch(hideLoading())
   }
